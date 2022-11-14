658395308300a232f23b24b01f4e0847516f71da
root <root@rasp2.dahoam>
2022-11-14 19:16:45 +0100

    first working prototype

---
+++ server.js
@@ -0,0 +1,48 @@
+// create an empty modbus client
+const ModbusRTU = require("modbus-serial");
+const client = new ModbusRTU();
+
+const mqtt = require("mqtt");
+const mqttClient = mqtt.connect(process.env.MQTT_SERVER, {"username": process.env.MQTT_USER, "password": process.env.MQTT_PWD});
+const mqttTopic = "deye-inverter/";
+
+let timeoutModbusRead = null;
+
+
+// open connection to a serial port
+client.connectRTUBuffered(process.env.DEVICE || "/dev/ttyUSB0", { baudRate: process.env.BOUDRATE || 9600 });
+client.setID(process.env.CLIENTID || 1);
+client.setTimeout(500);
+
+
+const registers = [
+    {"address": 607, "topic": "grid/power/total", "unit": 1},
+    {"address": 609, "topic": "grid/frequency", "unit": 0.01},
+    {"address": 637, "topic": "inverter/power/total", "unit": 1},
+    {"address": 643, "topic": "load/power/total", "unit": 1},
+    {"address": 672, "topic": "pv/power/pv1", "unit": 1},
+    {"address": 673, "topic": "pv/power/pv2", "unit": 1}
+];
+
+
+const startRegister = 600;
+
+setInterval(function() {
+
+    client.readHoldingRegisters(startRegister, 90, function(err, data) {
+
+        for (register of registers) {
+            let index = register.address - startRegister;
+            let value = ((data.data[index] << 16) >> 16) * register.unit;
+
+            mqttClient.publish(mqttTopic + register.topic, value.toString());
+        }
+
+        // calculate power total
+        let pvPowerTotal = data.data[72] + data.data[73];
+        mqttClient.publish(mqttTopic + "pv/power/total", pvPowerTotal.toString());
+
+    });
+
+}, 1000);
+
